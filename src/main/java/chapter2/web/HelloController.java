941bda339bcf885dd1f956464f41280fc67383b5
kimdaeseon <kimdaeseon@khu.ac.kr>
2021-03-09 02:14:41 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹서비스 2.2 까지

---
+++ src/main/java/chapter2/web/HelloController.java
@@ -0,0 +1,13 @@
+package chapter2.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
