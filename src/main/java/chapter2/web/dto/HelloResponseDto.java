4d663326c73c19f71b04edda9311143825f0cda3
kimdaeseon <kimdaeseon@khu.ac.kr>
2021-03-09 20:27:00 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹서비스 2장 완료

---
+++ src/main/java/chapter2/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package chapter2.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
